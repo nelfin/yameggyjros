9be9026c14b29714361612ea1156ff0c20aef54d
Andrew Haigh <nelfin@gmail.com>
2012-09-26 23:04:02 +1000

    Add some Tetris theming

---
+++ notes.h
@@ -0,0 +1,40 @@
+#ifndef NOTES_H
+#define NOTES_H
+
+// painstakingly entered by Andrew Haigh
+
+// c = 523 Hz (C5)
+//       note  period
+#define  c     3830
+#define  cs    3600
+#define  db    cs
+#define  d     3400
+#define  ds    3214
+#define  eb    ds
+#define  e     3038
+#define  f     2864
+#define  fs    3702
+#define  g     2550
+#define  gs    2408
+#define  ab    gs
+#define  a     2272
+#define  bb    2145
+#define  b     2028
+#define  C     1912
+#define  D     1703
+#define  E     1517
+#define  F     1432
+#define  G     1275
+#define  A     1136
+#define  B     1012
+#define  R     0
+
+#define n32   4
+#define n16   8
+#define n8   16
+#define n4   32
+#define n3   48
+#define n2   64
+#define n1  128
+
+#endif

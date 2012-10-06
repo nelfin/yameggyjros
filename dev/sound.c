d98431e4ca3513d240f7ea98ef6a73e405902a7d
Andrew Haigh <nelfin@gmail.com>
2012-10-06 20:18:10 +1000

    Add sound routines

--- dev/sound.c
+++ dev/sound.c
@@ -1,1 +1,21 @@
 #include "sound.h"
+
+void enable_piezo() {
+    PIEZO_DDR |= PIEZO_ON;
+}
+
+void disable_piezo() {
+    PIEZO_DDR &= PIEZO_OFF;
+}
+
+void piezo_on() {
+    PIEZO_DR |= PIEZO_ON;
+}
+
+void piezo_off() {
+    PIEZO_DR &= PIEZO_OFF;
+}
+
+void piezo_toggle() {
+    PIEZO_DR ^= PIEZO_ON;
+}
--- dev/sound.h
+++ dev/sound.h
@@ -1,6 +1,22 @@
 #ifndef SOUND_H
 #define SOUND_H
 
+#include <avr/io.h>
 
+#define PIEZO       PB1
+
+// Piezo data register
+#define PIEZO_DR    PORTB
+// Piezo data direction register
+#define PIEZO_DDR   DDRB
+
+#define PIEZO_ON    (1 << PIEZO)
+#define PIEZO_OFF   (~(1 << PIEZO))
+
+void enable_piezo();
+void disable_piezo();
+void piezo_on();
+void piezo_off();
+void piezo_toggle();
 
 #endif

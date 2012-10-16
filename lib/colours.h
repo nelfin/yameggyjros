ea86534bbc1491b8d0b55c43c9f4fe1ce23bb7e1
Andrew Haigh <nelfin@gmail.com>
2012-10-16 16:55:20 +1100

    Add colour macros
    This should make it a little easier to use. Each can paint a single
    pixel (7 bits). To make the "dark" variants of the colours (not unique,
    but you can get colours like #7f7f7f this way), you bitwise and vDARK.
    For example #7f7f7f = cffffff & VDARK.

---
+++ lib/colours.h
@@ -0,0 +1,73 @@
+#ifndef COLOURS_H
+#define COLOURS_H
+
+/* Colours */
+#define c000000 0x40u /* 0b1000000 */
+#define c550000 0x41u /* 0b1000001 */
+#define caa0000 0x42u /* 0b1000010 */
+#define cff0000 0x43u /* 0b1000011 */
+#define c005500 0x44u /* 0b1000100 */
+#define c555500 0x45u /* 0b1000101 */
+#define caa5500 0x46u /* 0b1000110 */
+#define cff5500 0x47u /* 0b1000111 */
+#define c00aa00 0x48u /* 0b1001000 */
+#define c55aa00 0x49u /* 0b1001001 */
+#define caaaa00 0x4au /* 0b1001010 */
+#define cffaa00 0x4bu /* 0b1001011 */
+#define c00ff00 0x4cu /* 0b1001100 */
+#define c55ff00 0x4du /* 0b1001101 */
+#define caaff00 0x4eu /* 0b1001110 */
+#define cffff00 0x4fu /* 0b1001111 */
+#define c000055 0x50u /* 0b1010000 */
+#define c550055 0x51u /* 0b1010001 */
+#define caa0055 0x52u /* 0b1010010 */
+#define cff0055 0x53u /* 0b1010011 */
+#define c005555 0x54u /* 0b1010100 */
+#define c555555 0x55u /* 0b1010101 */
+#define caa5555 0x56u /* 0b1010110 */
+#define cff5555 0x57u /* 0b1010111 */
+#define c00aa55 0x58u /* 0b1011000 */
+#define c55aa55 0x59u /* 0b1011001 */
+#define caaaa55 0x5au /* 0b1011010 */
+#define cffaa55 0x5bu /* 0b1011011 */
+#define c00ff55 0x5cu /* 0b1011100 */
+#define c55ff55 0x5du /* 0b1011101 */
+#define caaff55 0x5eu /* 0b1011110 */
+#define cffff55 0x5fu /* 0b1011111 */
+#define c0000aa 0x60u /* 0b1100000 */
+#define c5500aa 0x61u /* 0b1100001 */
+#define caa00aa 0x62u /* 0b1100010 */
+#define cff00aa 0x63u /* 0b1100011 */
+#define c0055aa 0x64u /* 0b1100100 */
+#define c5555aa 0x65u /* 0b1100101 */
+#define caa55aa 0x66u /* 0b1100110 */
+#define cff55aa 0x67u /* 0b1100111 */
+#define c00aaaa 0x68u /* 0b1101000 */
+#define c55aaaa 0x69u /* 0b1101001 */
+#define caaaaaa 0x6au /* 0b1101010 */
+#define cffaaaa 0x6bu /* 0b1101011 */
+#define c00ffaa 0x6cu /* 0b1101100 */
+#define c55ffaa 0x6du /* 0b1101101 */
+#define caaffaa 0x6eu /* 0b1101110 */
+#define cffffaa 0x6fu /* 0b1101111 */
+#define c0000ff 0x70u /* 0b1110000 */
+#define c5500ff 0x71u /* 0b1110001 */
+#define caa00ff 0x72u /* 0b1110010 */
+#define cff00ff 0x73u /* 0b1110011 */
+#define c0055ff 0x74u /* 0b1110100 */
+#define c5555ff 0x75u /* 0b1110101 */
+#define caa55ff 0x76u /* 0b1110110 */
+#define cff55ff 0x77u /* 0b1110111 */
+#define c00aaff 0x78u /* 0b1111000 */
+#define c55aaff 0x79u /* 0b1111001 */
+#define caaaaff 0x7au /* 0b1111010 */
+#define cffaaff 0x7bu /* 0b1111011 */
+#define c00ffff 0x7cu /* 0b1111100 */
+#define c55ffff 0x7du /* 0b1111101 */
+#define caaffff 0x7eu /* 0b1111110 */
+#define cffffff 0x7fu /* 0b1111111 */
+
+/* Colour values */
+#define vDARK   0x3f  /* 0b0111111 */
+
+#endif

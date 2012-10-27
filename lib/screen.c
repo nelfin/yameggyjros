b7625eb8fba6a2b8236ec1b25e31f6f420e1abed
Andrew Haigh <nelfin@gmail.com>
2012-10-27 12:37:39 +1100

    Add simple screen library
    This is basically a copy-paste of the fill_* functions we've been using
    in all of the screen code.

---
+++ lib/screen.c
@@ -0,0 +1,19 @@
+#include "../dev/leds.h"
+
+static inline void fill_screen(uint8_t colour) {
+    uint8_t i, j;
+    for (i = 0; i < 8; i++) {
+        for (j = 0; j < 8; j++) {
+            rgb_screen[i][j] = (pixel_t) colour;
+        }
+    }
+}
+
+static inline void fill_column(uint8_t col, uint8_t colour) {
+    uint8_t j;
+    for (j = 0; j < 8; j++) {
+        rgb_screen[col][j] = (pixel_t) colour;
+    }
+}
+
+

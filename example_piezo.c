64178ad9c755e2a6fe325ab094359f4a29bf17bc
Andrew Haigh <nelfin@gmail.com>
2012-10-27 15:16:05 +1100

    Refactor example_piezo.c
    Uses dev/keys now and has proper ms duration values for a tempo of
    120BPM.

--- example_piezo.c
+++ example_piezo.c
@@ -1,80 +1,75 @@
-#include <avr/io.h>
-
 #include "dev/leds.c"
 #include "dev/sound.c"
+#include "dev/keys.c"
+
 #include "lib/music.c"
 
+/* At 120 BPM, one crochet = 500ms */
 uint16_t tetris[] = {
-    tB4,   1712,
-    tGs4,  856,
-    tA4,   856,
-    tB4,   856,
-    tD5,   428,
-    tC5,   428,
-    tA4,   856,
-    tGs4,  856,
-    tE4,   1712,
-    tE4,   856,
-    tA4,   856,
-    tC5,   1712,
-    tB4,   856,
-    tA4,   856,
-    tGs4,  856,
-    tE4,   856,
-    tGs4,  856,
-    tA4,   856,
-    tB4,   1712,
-    tC5,   1712,
-    tA4,   1712,
-    tE4,   1712,
-    tE4,   3428,
-    1,     856,
-    tF4,   1712,
-    tA4,   856,
-    tC5,   856,
-    tC5,   428,
-    tC5,   428,
-    tB4,   856,
-    tA4,   856,
-    tG4,   2572,
-    tE4,   856,
-    tG4,   856,
-    tA4,   428,
-    tG4,   428,
-    tF4,   856,
-    tE4,   856,
-    tGs4,  856,
-    tE4,   856,
-    tGs4,  856,
-    tA4,   856,
-    tB4,   1712,
-    tC5,   1712,
-    tA4,   1712,
-    tE4,   1712,
-    tE4,   1712
+    tB4,   500,
+    tGs4,  250,
+    tA4,   250,
+    tB4,   250,
+    tD5,   125,
+    tC5,   125,
+    tA4,   250,
+    tGs4,  250,
+    tE4,   500,
+    tE4,   250,
+    tA4,   250,
+    tC5,   500,
+    tB4,   250,
+    tA4,   250,
+    tGs4,  250,
+    tE4,   250,
+    tGs4,  250,
+    tA4,   250,
+    tB4,   500,
+    tC5,   500,
+    tA4,   500,
+    tE4,   500,
+    tE4,   1000,
+    1,     250,
+    tF4,   500,
+    tA4,   250,
+    tC5,   250,
+    tC5,   125,
+    tC5,   125,
+    tB4,   250,
+    tA4,   250,
+    tG4,   750,
+    tE4,   250,
+    tG4,   250,
+    tA4,   125,
+    tG4,   125,
+    tF4,   250,
+    tE4,   250,
+    tGs4,  250,
+    tE4,   250,
+    tGs4,  250,
+    tA4,   250,
+    tB4,   500,
+    tC5,   500,
+    tA4,   500,
+    tE4,   500,
+    tE4,   500
 };
 
 #define SONG (sizeof(tetris) / sizeof(uint16_t))
 
 int main(void) {
     uint16_t i;
-    uint8_t keyp;
-
-    DDRC = 0;
-    PORTC = 255U;
 
     initialise_screen();
+    initialise_keys();
     enable_sound();
 
     for (;;) {
-        keyp = (~(PINC) & 0x01U);
-        if (keyp) {
+        if (get_next_key() == B_LEFT) {
             for (i = 0; i < SONG; i += 2) {
                 play_tone(tetris[i], (float) tetris[i+1]);
                 while (is_sound_playing());
             }
-        } else {
-            for (i = 0; i < 10000; i++);
         }
     }
 

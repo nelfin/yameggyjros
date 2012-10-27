a7a2d9e034bfdd15967cc78756572d5beb824277
Andrew Haigh <nelfin@gmail.com>
2012-10-27 11:44:23 +1100

    Make screen example more impressive

--- example_screen.c
+++ example_screen.c
@@ -1,15 +1,7 @@
-#include "lib/colours.h"
-
-/* Include screen code */
 #include "dev/leds.c"
 
-void delay(uint16_t ms) {
-   uint16_t i,j;
-   uint16_t loop = F_CPU / 17000;  // this is approximate and needs calabration.
-   for (i=0;i<ms;i++) {
-      for (j=0;j<loop;j++);
-   }
-}
+#include "lib/delay.c"
+#include "lib/colours.h"
 
 static inline void fill_screen(uint8_t colour) {
     uint8_t i, j;
@@ -26,49 +18,41 @@ static inline void fill_column(uint8_t col, uint8_t colour) {
         rgb_screen[col][j] = (pixel_t) colour;
     }
 }
+
+#define DELAY 50
+#define WASH_SCREEN(colour) \
+    for (col = 0; col < 8; col++) { \
+        fill_column(col, colour); \
+        for (i = 0; i < DELAY; i++) { \
+            delay_us(20); \
+        } \
+    }
+
 int main(void) {
     uint8_t lights = 0x1u;
     uint8_t col;
+    uint8_t i;
 
     initialise_screen();
 
-    while (1) {
-        for (col = 0; col < 8; col++) {
-            status_lights = lights;
-            lights = (lights << 1) | (lights >> 7);
-            fill_column(col, cff0000);
-            delay(20);
-        }
-        for (col = 0; col < 8; col++) {
-            status_lights = lights;
-            lights = (lights << 1) | (lights >> 7);
-            fill_column(col, cffff00);
-            delay(20);
-        }
-        for (col = 0; col < 8; col++) {
-            status_lights = lights;
-            lights = (lights << 1) | (lights >> 7);
-            fill_column(col, c00ff00);
-            delay(20);
-        }
-        for (col = 0; col < 8; col++) {
-            status_lights = lights;
-            lights = (lights << 1) | (lights >> 7);
-            fill_column(col, c00ffff);
-            delay(20);
-        }
-        for (col = 0; col < 8; col++) {
-            status_lights = lights;
-            lights = (lights << 1) | (lights >> 7);
-            fill_column(col, c0000ff);
-            delay(20);
-        }
-        for (col = 0; col < 8; col++) {
-            status_lights = lights;
-            lights = (lights << 1) | (lights >> 7);
-            fill_column(col, cff00ff);
-            delay(20);
-        }
+    for (;;) {
+        WASH_SCREEN(cff0000);
+        WASH_SCREEN(cffff00);
+        WASH_SCREEN(c00ff00);
+        WASH_SCREEN(c00ffff);
+        WASH_SCREEN(c0000ff);
+        WASH_SCREEN(cff00ff);
+        status_lights = lights;
+        lights = (lights << 1) | (lights >> 7);
+
+        WASH_SCREEN(c550000);
+        WASH_SCREEN(c555500);
+        WASH_SCREEN(c005500);
+        WASH_SCREEN(c005555);
+        WASH_SCREEN(c000055);
+        WASH_SCREEN(c550055);
+        status_lights = lights;
+        lights = (lights << 1) | (lights >> 7);
     }
 
     return 0;
